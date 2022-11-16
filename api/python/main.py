764210e7c95912e1c6deee5aa945fba968e56630
Maxime  Brun <gommugommu@MacBook-Air-de-Maxime.local>
2022-11-16 15:57:11 +0100

    changement chemin file python & nomenclature

--- api/python/main.py
+++ api/python/main.py
@@ -6,16 +6,16 @@ def main():
     a = API.SPARQLCall()
     fct = sys.argv[1]
 
-    if fct == "search":
+    if fct == "0":
         # Resultat de recherche
         print(a.get_result_search(sys.argv[2]))
-    elif fct == "ofseries":
+    elif fct == "1":
         # Récupérer les films d'une série
         print(a.get_all_movies_of_serie(sys.argv[2]))
-    elif fct == "sameseries":
+    elif fct == "2":
         # Récupérer les films d'une même série qu'un film donné
         print(a.get_all_films_from_same_serie(sys.argv[2]))
-    elif fct == "film":
+    elif fct == "3":
         # Récupérer les films d'une même série qu'un film donné
         print(a.get_Film(sys.argv[2]))
 
