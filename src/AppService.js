6dfe22470aa40d94071f94bfa819c0ed9a36b347
Noelbv <noe.le-blavec@insa-lyon.fr>
2022-11-16 10:23:31 +0100

    yoyoyoy

--- src/AppService.js
+++ src/AppService.js
@@ -10,15 +10,44 @@
 //     "X-Requested-With": "fetch",
 //   },
 // });
+
+
+
 class ApiService {
-  // EXEMPLE GET
-  // static getDataAutocompletion = async (currentInput, type, country) => {
-  //     const options = getRequestOptionsGoogleAPI();
-  //     const response = await fetch(
-  //       getAutocompleteUrl(currentInput, type, country),
-  //       options,
-  //       { signal: AbortSignal.timeout(2000) },
-  //     );
-  //     return response.json();
-  //   };
+
+  static getDataAutocompletion = async () => {
+    
+  }
 }
+
+const express=require('express');
+const app=express();
+ 
+//Import PythonShell module.
+const {PythonShell} =require('python-shell');
+ 
+//Router to handle the incoming request.
+app.get("/", (req, res, next)=>{
+    //Here are the option object in which arguments can be passed for the python_test.js.
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+          scriptPath: './src/Python', //If you are having python_test.py script in same folder, then it's optional.
+        args: ['0', 'Vin'] //An argument which can be accessed in the script using sys.argv[1]
+    };
+     
+ 
+    PythonShell.run('main.py', options, function (err, result){
+          if (err) throw err;
+          // result is an array consisting of messages collected
+          //during execution of script.
+          console.log('result: ', JSON.parse(result));
+          res.send(JSON.parse(result))
+    });
+});
+ 
+//Creates the server on default port 8000 and can be accessed through localhost:8000
+const port=8000;
+app.listen(port, ()=>console.log(`Server connected to ${port}`));
+
+
