332c2b6d3a2d4d192d9cc0756e8d841daa99d825
Noelbv <noe.le-blavec@insa-lyon.fr>
2022-11-16 10:35:55 +0100

    clean python

--- Python/Human.py
+++
@@ -1,8 +0,0 @@
-class Human:
-    def __init__(self, name, sex, image, country, birth):
-        self.name = name
-        self.sex = sex
-        self.image = image
-        self.country = country
-        self.birth = birth
-        self.occupation = []
--- Python/ProductionCompany.py
+++
@@ -1,5 +0,0 @@
-class ProductionCompany:
-    def __init__(self, title, founded_by, country):
-        self.title = title
-        self.founded_by = founded_by
-        self.country = country
