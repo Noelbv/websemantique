9260f4cdbadd79b01fff8b7d8446a1ed19029199
yboady <94382634+yboady@users.noreply.github.com>
2022-11-19 18:03:24 +0100

    FilmPage front clean

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -3,7 +3,7 @@ import SearchInput from './SearchInput';
 import SearchButton from './SearchButton';
 const NavBar = (props) => {
     return (
-        <div className="bg-blacked fixed flex h-24 flex-row items-center w-full z-40">
+        <div className="bg-blacked fixed flex h-24 flex-row items-center w-full z-40 ">
                 <h1 className="text-3xl text-gray-200 font-bold mb-5 mr-16 font-poppins">
                     Netfl'IF
                 </h1>
