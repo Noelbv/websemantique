3bca76cf21dac02d65a76f3b9919c14cd1f462cf
yboady <94382634+yboady@users.noreply.github.com>
2022-11-20 10:01:32 +0100

    FilmPage Dynamique

--- app/src/routes/FilmPage.js
+++ app/src/routes/FilmPage.js
@@ -1,42 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Rating from "@mui/material/Rating";
+import ApiService from "../AppService";
 
 import HarryPotterImage from "../misc/images/HP5.jpg";
 
 import NavBar from "../components/NavBar";
 // page qui donne les infos d'un film
 const FilmPage = () => {
+
+  const [responseFilm, setResponseFilm] = useState("");
+  const [isDataFetched, setIsDataFetched] = useState(false);
+
+  useEffect(() => {
+    ApiService.getMovie("wd:Q24871")
+      .then((res) => {
+        console.log(res);
+        setResponseFilm(res);
+        if(!isDataFetched) {
+          setIsDataFetched(true);
+        }
+      });
+  }, []);
+
+  const casting = responseFilm.cast_member;
+  //const director = responseFilm.director;
+  const screenwriter = responseFilm.screenwriter;
+
   // const [linkImage, setLinkImage] = useState("");
   // setLinkImage("https://static01.nyt.com/images/2013/12/27/multimedia/movies-wolf-12272013/movies-wolf-12272013-superJumbo.jpg")
   return (
     <div className="bg-blacked font-poppins flex-col items-stretch h-screen text-gray-200  overflow-y-auto hide-scroll-bar">
       <NavBar />
       <div className="pt-24">
-        <div id="header" className="flex flex-row items-center justify-center bg-cover bg-center relative w-100 h-72 bg-white overflow-hidden bg-[url('https://static01.nyt.com/images/2013/12/27/multimedia/movies-wolf-12272013/movies-wolf-12272013-superJumbo.jpg')]">
+      {isDataFetched ? (
+      <>
+        <div
+          id="header"
+          className="flex flex-row items-center justify-center bg-cover bg-center relative w-100 h-72 bg-white overflow-hidden"
+          style={{ backgroundImage: `url(${responseFilm.photo})` }}
+        >
           <h1 className="relative z-10 font-bold font-poppins text-6xl">
-            Le loup de Wall Street
+            {responseFilm.title}
           </h1>
           <div className="absolute bg-black opacity-50 w-full h-full">
           </div>
         </div>
         <div id="contenu" className="pt-12 pl-24 pr-24 pb-24">
           <div className="font-semibold text-xl mt-4 mb-2">Synopsis</div>
-          <div className="text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <div className="text-sm max-h-24 overflow-hidden">
+            {responseFilm.resume}
           </div>
-          <div className="font-semibold text-xl mt-4 mb-2">Acteurs</div>
-          <h2 className="text-sm mb-2">Emma Watson</h2>
-          <h2 className="text-sm mb-2">Daniel Radcliffe</h2>
-          <h2 className="text-sm mb-2">Yanice Baody</h2>
+          <div className="flex flex-row">
+            <div className="flex flex-col w-1/3 order-2">
+              <h3 className="font-semibold text-xl mt-4 mb-2">Director</h3>
+              <h4 className="text-sm">{responseFilm.director[0]}</h4>
+            </div>
+            <div className="flex flex-col w-1/3 order-3">
+              <h3 className="font-semibold text-xl mt-4 mb-2">Screenwriter</h3>
+              <ul className="text-sm">
+                {screenwriter.map(s => (<li>{s[0]}</li>))}
+              </ul>
+            </div>
+            <div className="flex flex-col w-1/3 order-1">
+              <h3 className="font-semibold text-xl mt-4 mb-2 ">Cast members</h3>
+              <ul className="text-sm">
+                {casting.map(s => (<li>{s[0]}</li>))}
+              </ul>
+            </div>
+          </div>
           <div className="font-semibold text-xl mt-4 mb-2">Avis</div>
           <Rating name="read-only" value={4.4} precision={0.25} readOnly />
         </div>
+      </>
+      ) : (
+        <>
+            <div className="font-poppins text-white font-medium text-xl">
+              Data Processing
+            </div>
+          </>
+      )}
       </div>
     </div>
   );
