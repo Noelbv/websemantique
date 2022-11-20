fb271f987144e8066ab0883c4801088a8b66f78f
yboady <94382634+yboady@users.noreply.github.com>
2022-11-20 14:21:56 +0100

    FilmBox

--- app/src/components/FilmBox.js
+++ app/src/components/FilmBox.js
@@ -7,24 +7,24 @@ const FilmBox = (props) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate(`/film/${props.title}`);
+    navigate(`/film/${props.idFilm}`);
   }
   
   return (
     <div   className="inline-block px-3">
       <button onClick={handleClick} className="group/edit relative w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
-        src={HarryPotterImage}
+        src={props.imageUrl}
         alt="film"
-        className="absolute rounded-lg "
+        className="absolute rounded-lg object-cover h-full w-full"
       />
       <div className="group-hover/edit:h-full transition-h duration-100 ease-in-out absolute opacity-80 bottom-0 bg-black w-full h-16"></div>
-      <div className="flex flex-col h-full justify-end px-2">
-        <p id="title" className="group-hover/edit:text-2xl transition-text duration-100 ease-in-out font-poppins font-semibold text-md text-white z-30 pl-4 pb-1">
+      <div className="flex flex-col h-full justify-end">
+        <p id="title" className="group-hover/edit:text-2xl transition-text duration-100 ease-in-out font-poppins font-semibold text-md text-white z-30 px-2 pb-1">
           {props.title}
         </p>
-        <p id="duration" className="group-hover/edit:text-xl transition-text duration-100 ease-in-out group-hover/edit:pb-16 transition-pb duration-100 ease-in-out font-poppins text-sm text-white z-20 pl-4 pb-1">
-          {props.duration}
+        <p id="duration" className="group-hover/edit:text-xl transition-text duration-100 ease-in-out group-hover/edit:pb-16 transition-pb duration-100 ease-in-out font-poppins text-sm font-semibold text-rose-900 z-20 pb-1">
+          {props.duration} min
         </p>
       </div>
     </button>
